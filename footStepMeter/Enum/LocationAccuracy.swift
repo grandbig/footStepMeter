fe852f6ef5756e14a612ba17feeb0adeb7069a5c
Takahiro Kato <takahiro.kato15@gmail.com>
2018-10-13 18:36:54 +0900

    Enumの追加

--- footStepMeter/Enum/LocationAccuracy.swift
+++ footStepMeter/Enum/LocationAccuracy.swift
@@ -7,20 +7,35 @@
 //
 
 import Foundation
+import CoreLocation
 
-/*
- 位置情報の取得精度
- */
-enum LocationAccuracy: Int {
-    case bestForNavigation = 0
+/// 位置情報の取得精度
+enum LocationAccuracy: String, CaseIterable {
+    case bestForNavigation
     case best
     case nearestTenMeters
     case hundredMeters
     case kilometer
     case threeKilometers
-    
-    static let defaultAccuracy = LocationAccuracy.bestForNavigation
-    init() {
-        self = LocationAccuracy.bestForNavigation
+
+    /// CLLocationAccuracyに変換する処理
+    ///
+    /// - Parameter accuracy: LocationAccuracyの値
+    /// - Returns: 変換したCLLocationAccuracy
+    func toCLLocationAccuracy(_ accuracy: LocationAccuracy) -> CLLocationAccuracy {
+        switch accuracy {
+        case .bestForNavigation:
+            return kCLLocationAccuracyBestForNavigation
+        case .best:
+            return kCLLocationAccuracyBest
+        case .nearestTenMeters:
+            return kCLLocationAccuracyNearestTenMeters
+        case .hundredMeters:
+            return kCLLocationAccuracyHundredMeters
+        case .kilometer:
+            return kCLLocationAccuracyKilometer
+        case .threeKilometers:
+            return kCLLocationAccuracyThreeKilometers
+        }
     }
 }
